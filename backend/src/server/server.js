aee8db74528c8c56798f6ecabca7debd76ef6c73
Vishal Sharma <sharmavs9205@gmail.com>
2024-06-01 00:06:42 +0530

    server.js updated

--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -6,12 +6,12 @@ import contactRoutes from "./routes/contactRoutes.js"
 
 connectDb();
 const app = express();
-const port=3000;
+const port=process.env.PORT || 3000;
 
 app.use(json());
 
 app.use(cors({
-    origin: 'https://portfolio-sigma-sand-36.vercel.app', // Allow requests from this origin
+    origin: 'http://localhost:3001', // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
 }));
